9b8df638c05bf6aca4317556f708f9adc51787f1
Maksim Shvechkov <shvechkov_maks@mail.ru>
2022-08-30 13:57:25 +0300

    added logic and output for classes Books and Employees

--- src/kz/attractor/java/lesson44/Books.java
+++ src/kz/attractor/java/lesson44/Books.java
@@ -4,24 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Books {
-    private Boolean inStock;
-    private Employees name;
-    List<Book> books = new ArrayList<Book>();
+
+    public int getRandom() {
+        return random;
+    }
+
+    private int random;
+    List<Book> books = new ArrayList<>();
+
+    public List<Book> getBooks() {
+        return books;
+    }
 
     public Books(){
-        books.add(new Book("sdasd", "dasdasd"));
-        books.add(new Book("sdad", "dasdasd"));
-        books.add(new Book("sdasd", "dasdasd"));
+        books.add(new Book("Pet cemetery", "das", true, null));
+        books.add(new Book("the little Prince", "dasdasd", true, null));
+        books.add(new Book("complete Java course", "dasdasd", true, null));
+    }
+
+    public void getBookByEmployee(){
+        bookRandomizer();
+        Employees employees = new Employees();
+        List<String> allEmployees = new ArrayList<>();
+        for (Employees.Employee e: employees.getEmployees()) {
+            allEmployees.add(e.getName());
+        }
+        Employees employees1 = new Employees();
+        books.set(random, new Book(null, null, false, allEmployees.get(employees1.getRandom())));
+    }
+
+    public void returnBook(){
+        for (Book b : books) {
+            books.set(random, new Book(b.bookName, b.description, b.inStock, b.name));
+        }
+    }
+
+    public void bookRandomizer(){
+        this.random = (int)(Math.random() * books.size());
     }
 
     public static class Book {
         private String bookName;
         private String description;
+        private Boolean inStock = false;
+        private String name;
 
-        public Book (String bookName, String description){
+        public Book (String bookName, String description, Boolean inStock, String name){
+            //this(bookName, description, inStock, name);
             this.bookName = bookName;
             this.description = description;
+            this.name = name;
+            this.inStock = inStock;
+        }
 
+        public boolean getInStock() {
+            return inStock;
+        }
+
+        public void setInStock(boolean inStock) {
+            this.inStock = inStock;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getBookName() {
+            return bookName;
+        }
+
+        public void setBookName(String bookName) {
+            this.bookName = bookName;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
         }
 
     }
--- src/kz/attractor/java/lesson44/Employees.java
+++ src/kz/attractor/java/lesson44/Employees.java
@@ -1,6 +1,121 @@
 package kz.attractor.java.lesson44;
 
-public class Employees {
-    String name;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Employees {
+    public int getRandom() {
+        return random;
+    }
+
+    private int random;
+    private Employee employee = new Employee("Maks", "Shvechkov");
+    private LocalDateTime currentDateTime = LocalDateTime.now();
+    private List<Employee> employees = new ArrayList<>();
+    Books books = new Books();
+    public Employees() {
+        employees.add(new Employee("Marco", getBook()));
+        employees.add(new Employee("Winston", "Duarte", getBook()));
+        employees.add(new Employee("Amos", "Burton", "'Timmy'", getBook()));
+        employees.get(2).setIsHavingBook(true);
+    }
+
+    public String getBook(){
+        List<String> takenBooks = new ArrayList<>();
+        for (Books.Book b : books.getBooks()) {
+            takenBooks.add(b.getBookName());
+        }
+        Books books1 = new Books();
+        if(takenBooks.get(books1.getRandom()) == null){
+            return null;
+        }
+        return takenBooks.get(books1.getRandom());
+    }
+    public void employeeRandomizer(){
+        this.random = (int)(Math.random() * employees.size());
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setUser(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LocalDateTime getCurrentDateTime() {
+        return currentDateTime;
+    }
+
+    public void setCurrentDateTime(LocalDateTime currentDateTime) {
+        this.currentDateTime = currentDateTime;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public static class Employee{
+        private String name;
+        private String lastName;
+        private String middleName = null;
+        private Boolean isHavingBook;
+        private String bookName;
+        public Employee(String name, String bookName) { this(name, null, null, bookName); }
+
+        public Employee(String name, String lastName, String bookName) {
+            this(name, lastName, null, bookName);
+        }
+
+        public Employee(String name, String lastName, String middleName, String book){
+            this.name = name;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.bookName = book;
+        }
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getMiddleName() {
+            return middleName;
+        }
+
+        public void setMiddleName(String middleName) {
+            this.middleName = middleName;
+        }
+
+        public boolean isHavingBook() {
+            return isHavingBook;
+        }
+
+        public void setIsHavingBook(boolean isHavingBook) {
+            this.isHavingBook = isHavingBook;
+        }
+
+        public String getBookName() {
+            return bookName;
+        }
+
+        public void setBookName(String bookName) {
+            this.bookName = bookName;
+        }
+    }
 }
